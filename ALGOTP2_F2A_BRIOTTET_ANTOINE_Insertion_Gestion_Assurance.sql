4466829138ef6622129a1a4f925f6d93cda05fb8
Morkiis <a.briottet@ludus-academie.com>
2017-06-01 15:47:11 +0200

    BDDBriottetAntoineF2A

---
+++ ALGOTP2_F2A_BRIOTTET_ANTOINE_Insertion_Gestion_Assurance.sql
@@ -0,0 +1,63 @@
+/*Table Accident*/
+ insert into ACCIDENT
+values ('1','','1500');
+insert into ACCIDENT
+values ('2','','452');
+insert into ACCIDENT
+values ('3','','150');
+insert into ACCIDENT
+values ('4','','100');
+ insert into ACCIDENT
+values ('5','','15');
+
+/*Table Client*/
+
+insert into CLIENT
+values ('1','BRIOTTET','15 rue Charles');
+insert into CLIENT
+values ('2','Loulou','23 rue fils');
+insert into CLIENT
+values ('3','Gazou','18 bd zaza');
+insert into CLIENT
+values ('4','Filibert','15 rue porte des étoiles');
+insert into CLIENT
+values ('5','rené','12 rue Charles');
+
+/*Table Contrat*/
+
+insert into CONTRAT
+values ('1','1','Voiture','16-08-2008');
+insert into CONTRAT
+values ('2','2','moto','14-05-2005');
+insert into CONTRAT
+values ('3','3','Scooter','16-12-2012');
+insert into CONTRAT
+values ('4','4','bateau','15-11-2004');
+insert into CONTRAT
+values ('4','4','avion','14-02-2017');
+
+/*Table Implication*/
+
+insert into IMPLICATION
+values ('1','1');
+insert into IMPLICATION
+values ('2','2');
+insert into IMPLICATION
+values ('3','3');
+insert into IMPLICATION
+values ('4','4');
+insert into IMPLICATION
+values ('4','5');
+
+/*Table Vehicule*/
+
+Insert into VEHICULE
+values ('1','Fiat','weekend','1999','120ch','1','1','1');
+Insert into VEHICULE
+values ('2','suzuki','rapide','2004','700cc','2','2','2');
+Insert into VEHICULE
+values ('3','peugeot','206','2002','120ch','3','3','3');
+Insert into VEHICULE
+values ('4','Boston','whaler','2008','120ch','4','4','4');
+Insert into VEHICULE
+values ('5','Airbus','A380','2012','3000ch','5','5','4');
